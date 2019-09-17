339da2771d01e780bd928420feee7beea2afd426
José Luis Olivares <joseluis_503@yahoo.com>
2019-09-17 15:09:36 -0600

    C# Client and Installer

---
+++ okioskclient/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace okioskclient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.btnConfig = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(883, 463);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.WebBrowser1_DocumentCompleted);
+            // 
+            // btnConfig
+            // 
+            this.btnConfig.Image = global::okioskclient.Properties.Resources.config;
+            this.btnConfig.Location = new System.Drawing.Point(12, 405);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(53, 46);
+            this.btnConfig.TabIndex = 1;
+            this.btnConfig.UseVisualStyleBackColor = true;
+            this.btnConfig.Click += new System.EventHandler(this.BtnConfig_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 463);
+            this.Controls.Add(this.btnConfig);
+            this.Controls.Add(this.webBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(920, 510);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "okioskmonitor  v.2.0.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button btnConfig;
+    }
+}
+
---
+++ okioskclient/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Cliente para monitoreo de Kioskos
+//Desarrollado por: José Luis Olivares
+//Email: joseluiss_503@hotmail.com
+//Linkedin: https://www.linkedin.com/in/jolivaress/
+//Fecha: Septiembre 2019
+
+namespace okioskclient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            //String fullPath = "C:/okiosk_monitor/agente_monitoreo.html";
+     
+            String  appPath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            appPath = Directory.GetParent(appPath).FullName;
+
+            String configPath = appPath; //path al archivo setServerIP
+            configPath += @"\okiosk_installer\app\config";
+            //validamos que la ruta a los archivos de configuración (y éstos) existan
+
+            if (System.IO.Directory.Exists(configPath))
+            {   
+                appPath += @"\okiosk_installer\app\agente_monitoreo.html";
+                String htmlPathOk = new Uri(appPath).LocalPath;
+                //htmlPathOk = htmlPathOk.Replace("\\", "/");
+                //webBrowser1.Navigate(fullPath);
+                //MessageBox.Show(htmlPathOk);
+              
+                webBrowser1.Navigate(htmlPathOk);
+            }
+            else
+            {
+                //MessageBox.Show("Revise que exista la ruta C:/okiosk_monitor/agente_monitoreo_official/");
+                webBrowser1.DocumentText =
+                    "<html><body style='background-color:#FFD2D2;'><div style='text-align: center; margin-top:20%;'>" +
+                    "<h3>No fue posible encontrar los archivos de configuración en la ruta "+configPath+"</h3>" +
+                    "</div></body></html>";
+            }
+        }
+
+        private void WebBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+           
+        }
+
+        static Boolean ValidarIP(String ip)
+        {
+            Regex ipExp = new Regex(@"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"); //Exp Regular par IP valida
+            if (ip.Length>=7) //longitud minima de una ip, Ex. 1.1.1.1
+            {
+                if (ipExp.IsMatch(ip))//validando patron de una ip
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        private void BtnConfig_Click(object sender, EventArgs e)
+        {
+            String ipCliente = Microsoft.VisualBasic.Interaction.InputBox("Escriba la Ip del cliente (este equipo):",
+                                "Ip a monitorear", "192.168.7.224");
+
+            ipCliente = ipCliente.Trim(); //borrando espacios
+
+            Regex ipExp = new Regex(@"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"); //Exp Regular par IP valida
+
+
+            String appPath = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            appPath = Directory.GetParent(appPath).FullName;
+            String jsFile =appPath+@"\okiosk_installer\app\config\setIPs.js";
+
+            if (ValidarIP(ipCliente)) //validamos la IP
+            {
+                string fileContent;
+
+                try
+                {
+                    fileContent = File.ReadAllText(jsFile);
+                    MessageBox.Show("IP Valida");
+                    String patIpCliente = @"cliente={IP:\'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b\'}";
+                    String newIpCliente = @"cliente={IP:'" + ipCliente + "'}";
+                    String patIpServer = @"servidor={IP:\'http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:3000\b\'}";
+                    String newContent = Regex.Replace(fileContent, patIpCliente, newIpCliente);
+
+                    //definiendo permisos de escritura en archivo
+                    FileIOPermission fPermission = new FileIOPermission(FileIOPermissionAccess.Write | FileIOPermissionAccess.Read, jsFile);
+
+                    try
+                    {
+                        String ipServer = Microsoft.VisualBasic.Interaction.InputBox("Escriba la Ip del Servidor de Monitoreo:",
+                                "Ip Servidor", "192.168.1.1");
+                        ipServer = ipServer.Trim();
+
+                        String newIpServer = @"servidor={IP:'http://" + ipServer + ":3000'}"; 
+
+                        if (ValidarIP(ipServer))// si la ip del server es válida
+                        {
+                            MessageBox.Show("IP Valida");
+                            String newContent2= Regex.Replace(newContent, patIpServer, newIpServer);
+
+                            //-----Para validar cambios y ver si se aplicaron
+                                //MessageBox.Show(fileContent);
+                                //MessageBox.Show(newContent2);
+                            //------------------------------------------------
+                            fPermission.Demand();
+                            File.WriteAllText(jsFile, newContent2);//Escribimos el nuevo archivo
+                            MessageBox.Show("¡IP's Guardadas correctamente! (Reinicie la apliación)");
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("¡IP del Servidor no válida!");
+                            return;
+                        }
+                    }
+                    catch (Exception)//SecurityException
+                    {
+                        MessageBox.Show(@"No tiene permisos para escribir directamente en .\config\setIPs.js" +
+                            Environment.NewLine + "Intente ejecutar la aplicación como administrador");
+                        throw;
+                    }
+
+                }
+                catch (Exception err)
+                {
+
+                    MessageBox.Show(err.ToString());
+                    throw;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("¡Formato de Ip introducido no es válido!");
+                return;
+            }
+
+          
+        }
+    }
+}
