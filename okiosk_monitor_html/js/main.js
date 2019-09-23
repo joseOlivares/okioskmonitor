6c177ba83c4d64d66caa76da34b026338fc0a921
José Luis Olivares <joseluis_503@yahoo.com>
2019-09-23 14:40:59 -0600

    eliminando funcion printer del cliente

--- okiosk_monitor_html/js/main.js
+++ okiosk_monitor_html/js/main.js
@@ -10,7 +10,7 @@
       var printerName="Zebra TTP 2030"; //Nombre del impresor instalado en el cliente
       var  clientIP=cliente.IP; //Ip del cliente a monitorear, leida desde seIPs.js
       var  ipAsID=clientIP.replace(/[.]/g,'');//usando ip sin puntos como identificador
-      var  serverIP=servidor.IP; //leida desde setIps.js
+	  var  serverIP=servidor.IP; //leida desde setIps.js
 
       var prStatus=-1, strPrinterStatus=-1, prExtendedPrinterstatus=-1;
       var prDetectedErrorState=-1,strDetectedErrorstate=-1, prExtendedDetectedErrorState=-1, strExtendedDetectedErrorState=-1;
@@ -36,135 +36,18 @@ var app={
 	            var p = e.item ();
 	            if (p.Default==true)       
 	            {
+					//Leyendo la data del printer Default, para enviarla al server
 		      		  printerName=p.caption;
 		              prStatus=p.printerStatus;
 		              prExtendedPrinterstatus=p.ExtendedPrinterStatus;
 		              prDetectedErrorState=p.DetectedErrorState;
 		              prExtendedDetectedErrorState=p.ExtendedDetectedErrorState;
-		              //estado de error detectado 0, estado de error extendido 0, 
-				    switch (prStatus) {
-				        case 1: 
-				          strPrinterStatus = "Other - Otro"; 
-				          break;    //prExtendedPrinterstatus  = wbemObject.ExtendedPrinterStatus 
-				        case 2: 
-				          strPrinterStatus = "Unknown - Desconocido";
-				          break; 
-				        case 3: 
-				          strPrinterStatus = "Idle - Sin Utilizar"; 
-				          break;
-				        case 4: 
-				          strPrinterStatus = "Printing - Imprimiendo"; 
-				          break;
-				        case 5: 
-				          strPrinterStatus = "Warmup - Calentando";
-				          break; 
-				        case 6: 
-				          strPrinterStatus= "Stopped printing - Impresion detenida";
-				          break; 
-				        case 7: 
-				          strPrinterStatus = "Offline - Fuera de Linea";
-				          break;       
-				     }   
-
-				    switch(prDetectedErrorState){  
-				        case 0: 
-				            strDetectedErrorstate=" Unknown - Desconocido";
-				            break; 
-				        case 1: 
-				            strDetectedErrorstate=" Other - Otro";
-				            break; 
-				        case 2: 
-				            strDetectedErrorstate=" NoError";
-				            break; 
-				        case 3: 
-				            strDetectedErrorstate=" Low Paper - Papel Bajo";
-				            break; 
-				        case 4: 
-				            strDetectedErrorstate=" NoPaper - Sin Papel";
-				            break; 
-				        case 5: 
-				            strDetectedErrorstate=" Low Toner - Papel Bajo ";
-				            break; 
-				        case 6: 
-				            strDetectedErrorstate=" NoToner - Sin Toner";
-				            break; 
-				        case 7: 
-				            strDetectedErrorstate=" Door Open - Puerta abierta";
-				            break; 
-				        case 8: 
-				            strDetectedErrorstate=" Jammed - Atascado";
-				            break; 
-				        case 9: 
-				            strDetectedErrorstate=" Offline - Fuera de Linea";
-				            break; 
-				        case 10: 
-				            strDetectedErrorstate=" Service Requested - Requiere Servicio";
-				            break; 
-				        case 11: 
-				            strDetectedErrorstate=" Output Bin Full - Papel en puerta de salida";
-				            break;
-				    }
-
-
-				    switch (prExtendedDetectedErrorState){
-				        case 0: 
-				            strExtendedDetectedErrorState=" Unknown - Desconocido";
-				            break; 
-				        case 1: 
-				            strExtendedDetectedErrorState=" Other - Otro";
-				            break; 
-				        case 2: 
-				            strExtendedDetectedErrorState=" No Error";
-				            break; 
-				        case 3: 
-				            strExtendedDetectedErrorState=" Low Paper -Papel Bajo";
-				            break; 
-				        case 4: 
-				            strExtendedDetectedErrorState=" No Paper - Sin Papel";
-				            break; 
-				        case 5: 
-				            strExtendedDetectedErrorState=" Low Toner - Papel Bajo";
-				            break; 
-				        case 6: 
-				            strExtendedDetectedErrorState=" No Toner - Sin Toner";
-				            break; 
-				        case 7: 
-				            strExtendedDetectedErrorState=" Door Open - Puerta abierta";
-				            break; 
-				        case 8: 
-				            strExtendedDetectedErrorState=" Jammed - atascado";
-				            break; 
-				        case 9: 
-				            strExtendedDetectedErrorState=" Service Requested - Requiere Servicio";
-				            break; 
-				        case 10: 
-				            strExtendedDetectedErrorState=" Output Bin Full - Papel en puerta de salida";
-				            break; 
-				        case 11: 
-				            strExtendedDetectedErrorState=" Paper Problem - Problema de Papel";
-				            break; 
-				        case 12: 
-				            strExtendedDetectedErrorState=" Cannot Print Page - No se puede imprimir la pagina";
-				            break; 
-				        case 13: 
-				            strExtendedDetectedErrorState=" User Intervantion Required - Requiere intervencion del Usuario";
-				            break; 
-				        case 14: 
-				            strExtendedDetectedErrorState=" Out of Memory - Sin memoria";
-				            break; 
-				        case 15: 
-				            strExtendedDetectedErrorState=" Server Unknown - Servidor desconocido"; 
-				            break;
-			    	}//end switch
-
+					  //estado de error detectado 0, estado de error extendido 0, 
 			    }//End IF		
 
 		            //alert('Corrida # '+pos);
 		            pos++;
-
-
 	      }//end For
-
 	},
 
 	showResults:function(){
