a8957527caf5aae25f80357284cf46c76fac3376
José Luis Olivares <joseluis_503@yahoo.com>
2019-09-27 08:36:36 -0600

    fix bug

--- okiosk_server/keys.js
+++ okiosk_server/keys.js
@@ -1,4 +1,4 @@
-var dbIp = require('./public/js/setServerDb.js/index.js').dbServer; //importando modulo donde esta la ip del servidor de BD
+var dbIp = require('./public/js/setServerDb.js').dbServer; //importando modulo donde esta la ip del servidor de BD
 
 /*
   host     : dbIp.dbServer,
