a103e8be237cf5615886c73ba9805f6ac1b39323
José Luis Olivares <joseluis_503@yahoo.com>
2019-10-04 14:35:02 -0600

    test

--- okiosk_server/public/js/main.js
+++ okiosk_server/public/js/main.js
@@ -229,7 +229,7 @@ var app={
 		//spining
 		$("#refreshOffline").removeClass( "fa fa-refresh" )
 		$("#refreshOffline").addClass( "fa fa-refresh fa-spin  fa-fw" )
-
+		debugger;
 		$("#divContenido").empty();
 		tplSource5='';
 		tplSource5=$("#tpl-equiposOffLine").html();
@@ -239,6 +239,7 @@ var app={
 
 
 		setTimeout(()=>{
+			debugger;
 			$("#refreshOffline").removeClass( "fa fa-refresh fa-spin  fa-fw" )
 			$("#refreshOffline").addClass( "fa fa-refresh" )
 		},900);	
